acfe76d9dbf648297018ca88b1532560aaf251d0
Prakhya <Prakhya@DESKTOP-AN5E1QN>
2020-04-26 19:37:04 +0530

    test file

--- src/test/java/com/qa/test/Demo1.java
+++ src/test/java/com/qa/test/Demo1.java
@@ -8,5 +8,15 @@ public class Demo1 {
 	public void add() {
 		System.out.println("Addition");
 	}
+	
+	@Test
+	public void add1() {
+		System.out.println("Addition");
+	}
+	
+	@Test
+	public void add2() {
+		System.out.println("Addition");
+	}
 
 }
